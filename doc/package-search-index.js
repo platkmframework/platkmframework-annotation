0d5a42e40dbf9ad3613c2f9a86af5dfc6ec79cdc
EDUARDO <eiglesias@platkmframework.org>
2023-08-29 17:30:35 +0200

    init commit

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.platkmframework.annotation"},{"l":"org.platkmframework.annotation.db"},{"l":"org.platkmframework.annotation.limit"},{"l":"org.platkmframework.annotation.security"}];updateSearchResults();
